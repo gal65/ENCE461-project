86533381a55611e7d3bcd552466e697cc345ade4
Liam Pribis <jackpribis@gmail.com>
2021-05-25 18:42:27 +1200

    Make new hat application with an attempt to import FreeRTOS

---
+++ src/apps/hat_freertos/main.c
@@ -0,0 +1,29 @@
+#include "target.h"
+
+#include "pio.h"
+#include "delay.h"
+
+#include "freertos_util.h"
+
+#define STACK_SIZE 512
+
+void blink_task(void*);
+
+int main(void) {
+    pio_config_set(LED1_PIO, PIO_OUTPUT_LOW);
+    pio_config_set(LED2_PIO, PIO_OUTPUT_LOW);
+
+    xTaskCreate(blink_task, "blink", STACK_SIZE, NULL, 2, NULL);
+
+    vTaskStartScheduler();
+}
+
+void blink_task(void* params) {
+
+    while(true) {
+        pio_output_set(LED1_PIO, true);
+        vTaskDelay(MS_TO_TICKS(500));
+        pio_output_set(LED1_PIO, false);
+        vTaskDelay(MS_TO_TICKS(500));
+    }
+}
