45c85769d7cae72ee9a766528e45b8415b078d78
Liam Pribis <jackpribis@gmail.com>
2021-05-27 15:18:34 +1200

    steering control

---
+++ src/apps/hat/sound.c
@@ -0,0 +1,17 @@
+// #include "sound.h"
+
+// #include "dac.h"
+
+// dac_cfg_t dac_config = {
+//     .channel = DAC_CHANNEL_0,
+//     .channels = ~0,
+//     .bits = 12,
+
+// }
+// void sound_init() {
+
+// }
+
+// void sound_play() {
+
+// }
